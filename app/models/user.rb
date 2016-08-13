bc69a4f38bb7e3d9f083fc049cc419ea64220409
Anti-k <romanslyzebniy@gmail.com>
2016-08-13 13:30:12 +0300

    set_admin for first registerd user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  before_save :set_admin
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true
-  validates :name, uniqueness: { case_sensitive: false }    
+         :recoverable, :rememberable, :trackable, :validatable  
+
+  validates :name, :admin, presence: true
+  validates :name, uniqueness: { case_sensitive: false } 
+
+
+
+  private
+  def set_admin
+    self.admin = User.count == 0
+  end   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,28 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :name }  
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it { should validate_presence_of :admin }
   it { should validate_uniqueness_of(:name).case_insensitive }
+
+  
+  describe 'set_admin' do     
+    before do
+      @user = User.new(name: 'jetRuby', email: 'jet@ruby.com', password: '123123') 
+      @user.send :set_admin
+      @user.save
+    end  
+    context 'first registered user' do     
+      it 'user is admin' do        
+        expect(@user.admin?).to be_truthy 
+      end
+    end
+    context 'second registerd user' do
+      another_user = User.new(name: 'jetRuby1', email: 'jet1@ruby.com', password: '123123') 
+        it 'another_user is not admin' do
+          another_user.send :set_admin
+          another_user.save
+          expect(another_user.admin?).to be_falsy 
+      end
+    end
+  end
 end
