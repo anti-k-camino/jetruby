ec48f4ae1dde878194a64db6a5ab005aa2a306f1
Anti-k <romanslyzebniy@gmail.com>
2016-08-13 16:26:10 +0300

    leftover files deleted

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,20 +1,24 @@
+require 'admin/users_controller'
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
   describe Admin::UsersController do
     describe 'GET #index' do
-      let(:users){ create_list(:user, 2) }
-      it 'populates an array of users' do             
+      sign_in_user
+      let(:example_user){ create :user }
 
+      before do
         get :index
-
-        expect(assigns(:users)).to match_array(users)
       end
 
-      it 'renders index view' do
-        get :index
-        expect(response).to render_template 'admin/index'
+      it 'populates an array of users' do 
+        expect(assigns(:users)).to match_array([@user, example_user])
       end
+
+      it 'renders index view' do        
+        expect(response).to render_template :index
+      end
+
     end
   end
 
